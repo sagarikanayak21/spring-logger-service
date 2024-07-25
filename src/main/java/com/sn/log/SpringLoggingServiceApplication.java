d997cfb1df800aa65e74da4dff47a65ffa0ba075
sagarika-nayak <sagarikanayak440@gmail.com>
2024-07-25 10:22:16 +0530

    Initial commit of spring-logger-service /n/nSN-8001

---
+++ src/main/java/com/sn/log/SpringLoggingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sn.log;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLoggingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLoggingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sn/log/SpringLoggingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sn.log;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLoggingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
